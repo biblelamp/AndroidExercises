ce5b0ef1c758a617d31b0179e80b7b6922ae9352
Sergey Iryupin <biblelamp@yandex.ru>
2016-08-14 13:36:44 +0400

    chapter 2 beer adviser

---
+++ Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -0,0 +1,32 @@
+package com.hfad.beeradviser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    // Call when the user clicks the button
+    public void onClickFindBeer(View view) {
+
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        List<String> brandsList = BeerExpert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+}
