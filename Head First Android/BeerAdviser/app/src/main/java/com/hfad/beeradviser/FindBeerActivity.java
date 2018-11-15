fbca19c39a0e0a376be4ef050e8749808152f88c
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-15 07:57:22 +0100

    added method onClickFindBeer

--- Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.hfad.beeradviser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
 
@@ -9,5 +12,12 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
     }
 }
