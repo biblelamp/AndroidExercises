1f15eedc99ffa50d8b4cc2f3589707acc2962458
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-15 08:03:21 +0100

    added new class

---
+++ Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/BeerExpert.java
@@ -0,0 +1,13 @@
+package com.hfad.beeradviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerExpert {
+
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<String>();
+        
+        return brands;
+    }
+}
