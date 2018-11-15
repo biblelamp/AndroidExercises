b764257dfa32b2b5604ffd30453115a7e8d7e428
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-15 07:50:16 +0100

    changed generated code

--- Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -1,9 +1,9 @@
 package com.hfad.beeradviser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class FindBeerActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
