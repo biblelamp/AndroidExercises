ffcabda0a3efb10dacd69cc5dae147d734665921
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-16 09:17:35 +0100

    added code

--- Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/BeerExpert.java
+++ Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/BeerExpert.java
@@ -7,7 +7,13 @@ public class BeerExpert {
 
     List<String> getBrands(String color) {
         List<String> brands = new ArrayList<String>();
-        
+        if (color.equals("amber")) {
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
         return brands;
     }
 }
