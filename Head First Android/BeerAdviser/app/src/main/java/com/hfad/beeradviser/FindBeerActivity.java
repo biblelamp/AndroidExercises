a15e08f0ca4340e99acff7e3b30f969f2e12f4dd
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-16 09:20:29 +0100

    finished task

--- Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ Head First Android/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -6,7 +6,10 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,12 @@ public class FindBeerActivity extends AppCompatActivity {
         TextView brands = (TextView) findViewById(R.id.brands);
         Spinner color = (Spinner) findViewById(R.id.color);
         String beerType = String.valueOf(color.getSelectedItem());
-        brands.setText(beerType);
+
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList){
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
     }
 }
