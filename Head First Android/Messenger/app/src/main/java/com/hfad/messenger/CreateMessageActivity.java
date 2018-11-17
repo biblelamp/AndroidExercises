4d639df53d346232f249d7ce723931d7155f0892
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-17 21:02:09 +0100

    init commit

---
+++ Head First Android/Messenger/app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+}
