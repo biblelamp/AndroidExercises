6e80740f4a5520b1b9014337b171b3b00bfa22e6
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-24 22:22:09 +0100

    changed set files a bit

--- SlovaZivota/app/src/androidTest/java/cz/bjb/slovazivota/ExampleInstrumentedTest.java
+++ SlovaZivota/app/src/androidTest/java/cz/bjb/slovazivota/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("cz.bjb.slovaivota", appContext.getPackageName());
+        assertEquals("cz.bjb.slovazivota", appContext.getPackageName());
     }
 }
