427d96f6749b551d589dbd01b0dd94e2658eb2b2
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-25 19:48:17 +0100

    added reading file from assets

--- SlovaZivota/app/src/main/java/cz/bjb/slovazivota/MainActivity.java
+++ SlovaZivota/app/src/main/java/cz/bjb/slovazivota/MainActivity.java
@@ -2,6 +2,11 @@ package cz.bjb.slovazivota;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = (TextView) findViewById(R.id.text);
+        text.setMovementMethod(new ScrollingMovementMethod());
+        text.setText(getStringFromAssetFile());
+    }
+
+    private String getStringFromAssetFile() {
+        byte[] buffer = null;
+        InputStream is;
+        try {
+            is = getAssets().open("2018/10/25.txt");
+            int size = is.available();
+            buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return new String(buffer);
     }
 }
