fc19af32f83ca979602252c5c02f927e9537ab1b
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-26 10:59:17 +0100

    added loading text by date

--- SlovaZivota/app/src/main/java/cz/bjb/slovazivota/MainActivity.java
+++ SlovaZivota/app/src/main/java/cz/bjb/slovazivota/MainActivity.java
@@ -1,5 +1,12 @@
 package cz.bjb.slovazivota;
 
+/**
+ * Java. Slova Života - přemýšlejte o Božím Slovu
+ *
+ * @author Sergey Iryupin
+ * @version 0.2 dated Nov 26, 2018
+ */
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -9,11 +16,15 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    private DateTool date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        date = new DateTool();
+        setTitle(date.toString());
 
         TextView textView = (TextView) findViewById(R.id.text);
         textView.setMovementMethod(new ScrollingMovementMethod());
@@ -23,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private String getStringFromAssetFile() {
         byte[] buffer = null;
         try {
-            InputStream is = getAssets().open("2018/10/25.txt");
+            InputStream is = getAssets().open(date.getFileName());
             buffer = new byte[is.available()];
             is.read(buffer);
             is.close();
