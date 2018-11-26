061c22fcffcc005e93e1bb8af100fbca8bab4d3c
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-26 13:48:58 +0100

    added 1st paging version

--- SlovaZivota/app/src/main/java/cz/bjb/slovazivota/MainActivity.java
+++ SlovaZivota/app/src/main/java/cz/bjb/slovazivota/MainActivity.java
@@ -4,18 +4,21 @@ package cz.bjb.slovazivota;
  * Java. Slova Života - přemýšlejte o Božím Slovu
  *
  * @author Sergey Iryupin
- * @version 0.2 dated Nov 26, 2018
+ * @version 0.2.1 dated Nov 26, 2018
  */
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+    private TextView textView;
     private DateTool date;
 
     @Override
@@ -26,20 +29,45 @@ public class MainActivity extends AppCompatActivity {
         date = new DateTool();
         setTitle(date.toString());
 
-        TextView textView = (TextView) findViewById(R.id.text);
+        textView = (TextView) findViewById(R.id.text);
         textView.setMovementMethod(new ScrollingMovementMethod());
+        textView.setOnTouchListener(this);
+
         textView.setText(getStringFromAssetFile());
     }
 
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        float x = 0;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (event.getX() > textView.getWidth()/2) {
+                    date.add(1);
+                } else {
+                    date.add(-1);
+                }
+                setTitle(date.toString());
+                textView.setText(getStringFromAssetFile());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+        return true;
+    }
+
     private String getStringFromAssetFile() {
+        InputStream is = null;
         byte[] buffer = null;
         try {
-            InputStream is = getAssets().open(date.getFileName());
+            is = getAssets().open(date.getFileName());
             buffer = new byte[is.available()];
             is.read(buffer);
             is.close();
         } catch (IOException ex) {
-            ex.printStackTrace();
+            return "Žádný text pro tento den.";
         }
         return new String(buffer);
     }
