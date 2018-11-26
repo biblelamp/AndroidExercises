a23a6d4bd6f3408839951931068f9dfb6f9e9286
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-26 09:07:37 +0100

    added date tool

---
+++ SlovaZivota/app/src/main/java/cz/bjb/slovazivota/DateTool.java
@@ -0,0 +1,37 @@
+package cz.bjb.slovazivota;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTool {
+    final private String[] months = {"ledna", "února", "března", "dubna", "května", "června",
+            "července", "srpna", "září", "října", "listopadu", "prosince"};
+    private Calendar calendar;
+
+    public DateTool() {
+        calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+    }
+
+    public int getDay() {
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public int getMonth() {
+        return calendar.get(Calendar.MONTH);
+    }
+
+    public int getYear() {
+        return calendar.get(Calendar.YEAR);
+    }
+
+    public String getFileName() {
+        return Integer.toString(getYear()) + "/" +
+                Integer.toString(getMonth()) + "/" +
+                Integer.toString(getDay()) + ".txt";
+    }
+
+    public String toString() {
+        return Integer.toString(getDay()) + ". " + months[getMonth()];
+    }
+}
