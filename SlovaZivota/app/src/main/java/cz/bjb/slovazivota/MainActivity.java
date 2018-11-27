a4be6a56041ea6c08b41af852c4a379054409849
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-27 12:50:17 +0100

    added 3rd version of page turning

--- SlovaZivota/app/src/main/java/cz/bjb/slovazivota/MainActivity.java
+++ SlovaZivota/app/src/main/java/cz/bjb/slovazivota/MainActivity.java
@@ -4,68 +4,130 @@ package cz.bjb.slovazivota;
  * Java. Slova Života - přemýšlejte o Božím Slovu
  *
  * @author Sergey Iryupin
- * @version 0.2.2 dated Nov 26, 2018
+ * @version 0.3 dated Nov 27, 2018
  */
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector.OnDoubleTapListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.view.ViewConfiguration;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements OnTouchListener {
+public class MainActivity extends AppCompatActivity implements
+        OnGestureListener, OnDoubleTapListener {
+
     private TextView textView;
     private DateTool date;
-    private float x, y;
+
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDetector = new GestureDetectorCompat(this,this);
+        mDetector.setOnDoubleTapListener(this);
+
         date = new DateTool();
         setTitle(date.toString());
 
         textView = (TextView) findViewById(R.id.text);
         textView.setMovementMethod(new ScrollingMovementMethod());
-        textView.setOnTouchListener(this);
-
-        textView.setText(getStringFromAssetFile());
+        textView.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mDetector.onTouchEvent(event);
+                return false;
+            }
+        });
+        textView.setText(getStringFromAssetFile(date.getFileName()));
     }
 
     @Override
-    public boolean onTouch(View view, MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                x = event.getX();
-                y = event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                if (Math.abs(y - event.getY()) < 10) {
-                    date.add((int) Math.signum(x - event.getX()));
-                    setTitle(date.toString());
-                    textView.setText(getStringFromAssetFile());
-                    return true;
-                }
-                break;
+    public boolean onTouchEvent(MotionEvent event){
+        if (this.mDetector.onTouchEvent(event)) {
+            return true;
         }
-        return false;
+        return super.onTouchEvent(event);
     }
 
-    private String getStringFromAssetFile() {
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        date.add((int) Math.signum(-velocityX));
+        setTitle(date.toString());
+        textView.setText(getStringFromAssetFile(date.getFileName()));
+        textView.scrollTo(0, 0);
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        date.setDate(new Date());
+        setTitle(date.toString());
+        textView.setText(getStringFromAssetFile(date.getFileName()));
+        textView.scrollTo(0, 0);
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
+    }
+
+    private String getStringFromAssetFile(String fileName) {
         InputStream is = null;
         byte[] buffer = null;
         try {
-            is = getAssets().open(date.getFileName());
+            is = getAssets().open(fileName);
             buffer = new byte[is.available()];
             is.read(buffer);
             is.close();
