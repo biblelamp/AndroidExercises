778f8ed6a0e1e1db452915bdb2306dbfbdcb375a
Sergey Iryupin <biblelamp@yandex.ru>
2023-05-28 18:17:22 +0200

    connect4 touch and draw

--- Connect4/app/src/main/java/cz/javageek/connect4/Connect4.java
+++ Connect4/app/src/main/java/cz/javageek/connect4/Connect4.java
@@ -3,6 +3,7 @@ package cz.javageek.connect4;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.media.metrics.Event;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -10,9 +11,15 @@ import android.view.View.OnTouchListener;
 
 public class Connect4 extends Activity implements OnTouchListener {
 
-    private char[][] table = new char[7][6];
+    static final int COL = 7;
+    static final int ROW = 6;
+    static final int WIN = 4;
+
+    private char[][] table = new char[COL][ROW];
 
     private DrawView drawView;
+
+    private boolean myTurn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,41 @@ public class Connect4 extends Activity implements OnTouchListener {
     }
 
     @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
+    public boolean onTouch(View view, MotionEvent event) {
+        float clickX = event.getX();
+        float dx = drawView.getWidth() / COL;
+
+        // колонка куда падает фишка
+        int col = (int) (clickX / dx);
+
+        // мы щелкнули по экрану
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            for (int row = ROW - 1; row >= 0; row--) {
+                if (table[col][row] == 0) {
+                    table[col][row] = myTurn ? 'x' : 'o';
+                    myTurn = ! myTurn;
+                    break;
+                }
+            }
+            drawView.invalidate();
+        }
+
+        // проверка победы
+        isWin('x');
+
+        return true;
+    }
+
+    private boolean isWin(char ch) {
+        for (int y = 0; y < ROW; y++) {
+            for (int x = 0; x < COL; x++) {
+
+            }
+        }
         return false;
+    }
+
+    private int countChar(int x, int y, char ch, int dx, int dy) {
+        return 0;
     }
 }
--- Connect4/app/src/main/java/cz/javageek/connect4/DrawView.java
+++ Connect4/app/src/main/java/cz/javageek/connect4/DrawView.java
@@ -1,5 +1,8 @@
 package cz.javageek.connect4;
 
+import static cz.javageek.connect4.Connect4.COL;
+import static cz.javageek.connect4.Connect4.ROW;
+
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -8,31 +11,40 @@ import android.view.View;
 public class DrawView extends View {
 
     private char[][] table;
+    private int xT;
+    private int yT;
+
+
 
     public DrawView(Connect4 mainActivity, char[][] table) {
         super(mainActivity);
         this.table = table;
     }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        float dx = getWidth()/7;
+        float dx = getWidth() / COL;
         float rd = (dx / 2) * 0.80f;
-        float top = getHeight()- dx * 6;
+        float top = getHeight()- dx * ROW;
 
         Paint p = new Paint();
         p.setColor(Color.BLUE);
         p.setAntiAlias(true);
 
         canvas.drawRect(0, top, getWidth(), getHeight(), p);
-        p.setColor(Color.WHITE);
+        //p.setColor(Color.WHITE);
         //p.setStyle(Paint.Style.STROKE);
 
-        for (int y = 0; y < 6; y++) {
-            for (int x = 0; x < 7; x++) {
-                //canvas.drawCircle();
+        for (int y = 0; y < ROW; y++) {
+            for (int x = 0; x < COL; x++) {
+                if (table[x][y] == 'x') {
+                    p.setColor(Color.RED);
+                } else if (table[x][y] == 'o') {
+                    p.setColor(Color.YELLOW);
+                } else {
+                    p.setColor(Color.WHITE);
+                }
                 canvas.drawCircle(x * dx + dx / 2, y * dx + top + dx / 2, rd, p);
             }
         }
