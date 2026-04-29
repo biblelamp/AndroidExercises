33e1550e5c8471e16e4be023093d524b070808ca
Sergey Iryupin <biblelamp@yandex.ru>
2026-04-29 12:14:32 +0200

    vitrazeArt app release 1.2 to Google Play

--- VitrazeArt/app/build.gradle.kts
+++ VitrazeArt/app/build.gradle.kts
@@ -9,10 +9,10 @@ android {
 
     defaultConfig {
         applicationId = "cz.vitrazeart.app"
-        minSdk = 26
+        minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
